0620d4d4bb54d22c8fe0e917c387c3de73da57d1
jorgeortiz41 <jorge.ortiz41@upr.edu>
2024-03-18 07:38:04 -0400

    removed globe header and added million lint

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import { GlobeHeader } from "@/components/globe-header";
-
 export default function Home() {
   return (
     <main className="dark flex flex-col items-center justify-between p-4">
-      <GlobeHeader />
+      <div className="text-7xl p-24 h-full">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">CodeLobby</span>
+      </div>
     </main>
   );
 }
